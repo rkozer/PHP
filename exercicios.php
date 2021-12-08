81f8591c8eb11e786666377f4fec915226cafd52
rkozer <ricardokozerski@gmail.com>
2021-12-07 23:56:55 -0300

    alteração 2

--- exercicios.php
+++ exercicios.php
@@ -4,23 +4,23 @@
     <meta charset="UTF-8">
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="recursos\CSS\estilo.css">
-    <title>Curso PHP</title>
+    <link rel="stylesheet" href="recursos\CSS\exercicios.css">
+    <title>Exercício</title>
 </head>
-<body>
+<body class="exercicio">
     <header class="cabecalho">
         <h1>Curso PHP</h1>
-        <h2>Índice dos Exercícios</h2>
+        <h2>Visualização do exercício</h2>
     </header>
+    <nav class="navegacao">
+        <a href=<?= "{$_GET['dir']}/{$_GET['file']}.php" ?> class="verde">Sem formatação</a>
+        <a href="index.php" class="vermelho">Voltar</a>
+    </nav>
     <main class="principal">
         <div class="conteudo">
-            <nav class="modulos">
-                <div class="modulo verde">
-                    <h3>Módulo 01</h3>
-                    <ul>
-                        <li><a href="exercicios.php?dir=teste&file=teste">Exercício A</a></li>
-                    </ul>
-                </div>
-            </nav>
+            <?php
+            include(__DIR__ . "/{$_GET['dir']}/{$_GET['file']}.php");
+        ?>                   
         </div>
     </main>
     <footer class="rodape">
