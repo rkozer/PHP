4c0918450886973ada7ab988fe5943ef06645098
rkozer <ricardokozerski@gmail.com>
2021-12-28 17:11:02 -0300

    alteração 8

--- controle/if_else.php
+++ controle/if_else.php
@@ -1,1 +1,51 @@
 <div class="titulo">If Else</div>
+
+<?php
+
+$idade = 30;
+$alt = 1.70;
+$nome = "Ricardo";
+
+if($idade == 30 && $alt == 1.70 && $nome == "Ricardo") {
+    echo 'Esse sou eu!';
+} else {
+    echo 'Não sou eu!';
+}
+
+echo "<p>Exemplo 'or', apenas uma condição precisa ser verdadeira<hr></p>";
+
+$idade2 = 30;
+$alt2 = 1.70;
+$nome2 = "Ricardo";
+
+if($idade2 == 31 or $alt2 == 1.60 or $nome2 == "Ricard") {
+    echo 'Parece comigo!';
+} else {
+    echo 'Não sou eu!';
+}
+
+echo "<p>Exemplo 'xor', duas condições verdadeira ou falsas retornam 'false'<hr></p>";
+
+$idade3 = 30;
+$alt3 = 1.70;
+
+if($idade3 == 30 xor $alt3 == 1.70) {
+    echo 'É um pouco parcido comigo!';
+} else {
+    echo 'Parece muito comigo!';
+}
+
+?>
+
+<style>
+p {
+    margin-bottom: 0px;
+    font-weight: bold;
+   
+}
+hr {
+    margin-top: 0px;
+    margin-bottom: -40px;
+
+}
+</style>
